fea331bc34feb0222233b455792181fc78841a70
RomanNft <118902504+RomanNft@users.noreply.github.com>
2024-09-26 22:51:00 +0300

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "aws_access_key" {
+    type    = string
+    default = " "  
+}
+
+variable "aws_secret_key" {
+    type    = string
+    default = " "  
+}
+
+// https://013689672842.signin.aws.amazon.com/console
