a23a5eafb8d2dca2adb3a16acbb857b4a01a780a
RomanNft <118902504+RomanNft@users.noreply.github.com>
2024-09-27 23:20:37 +0300

    Update sayt_jenkins_doer.sh

--- sayt_jenkins_doer.sh
+++ sayt_jenkins_doer.sh
@@ -2,3 +2,4 @@
 git clone -b main https://github.com/RomanNft/facebook.git > /var/log/terraform-init.log 2>&1
 cd facebook
 bash setup.sh >> /var/log/terraform-init.log 2>&1
+bash installJenkins.sh >> /var/log/terraform-init.log 2>&1
