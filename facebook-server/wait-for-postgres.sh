12e7908dee7e0452d682438bb9fc41e0a06bec07
RomanNft <118902504+RomanNft@users.noreply.github.com>
2024-09-27 14:52:04 +0300

    Update wait-for-postgres.sh

--- facebook-server/wait-for-postgres.sh
+++ facebook-server/wait-for-postgres.sh
@@ -8,7 +8,7 @@ cmd="$@"
 
 until PGPASSWORD=$POSTGRES_PASSWORD psql -h "$host" -U "postgres" -c '\q'; do
   >&2 echo "Postgres is unavailable - sleeping"
-  sleep 5
+  sleep 180
 done
 
 >&2 echo "Postgres is up - executing command"
