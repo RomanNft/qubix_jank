1e9492dcdb6a63f95982da32ce1bcfe54a870d7f
root <root@ip-172-31-22-26.eu-central-1.compute.internal>
2024-10-09 17:53:07 +0000

    Initial commit

--- facebook-client/vite.config.ts
+++ facebook-client/vite.config.ts
@@ -1,10 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173, // Порт, на якому буде працювати фронтенд
-    host: '0.0.0.0' // Дозволяє доступ з зовнішніх IP-адрес
-  }
-})
+  define: {
+    'process.env': process.env,
+  },
+});
--- facebook-server/Facebook_Api/Controllers/AuthenticationController.cs
+++ facebook-server/Facebook_Api/Controllers/AuthenticationController.cs
@@ -32,75 +32,115 @@ public class AuthenticationController(ISender mediatr,
     IConfiguration configuration,
     IUserAuthenticationService authenticationService,
     UserManager<UserEntity> userManager,
-    ICurrentUserService currentUserService)
+    ICurrentUserService currentUserService,
+    ILogger<AuthenticationController> logger)
     : ApiController
 {
     [HttpPost("register")]
-public async Task<IActionResult> RegisterAsync([FromForm] RegisterRequest request)
-{
-    var baseUrl = configuration.GetRequiredSection("HostSettings:ClientURL").Value;
-
-    byte[] image = null;
-    if (request.Avatar != null && request.Avatar.Length > 0)
+    public async Task<IActionResult> RegisterAsync([FromForm] RegisterRequest request)
     {
-        using MemoryStream memoryStream = new();
-        await request.Avatar.CopyToAsync(memoryStream);
-        image = memoryStream.ToArray();
+        logger.LogInformation($"Attempting to register user: {request.Email}");
+
+        var baseUrl = configuration.GetRequiredSection("HostSettings:ClientURL").Value;
+
+        byte[] image = null;
+        if (request.Avatar != null && request.Avatar.Length > 0)
+        {
+            using MemoryStream memoryStream = new();
+            await request.Avatar.CopyToAsync(memoryStream);
+            image = memoryStream.ToArray();
+        }
+
+        var authResult = await mediatr.Send(mapper
+            .Map<RegisterCommand>((request, baseUrl, image)));
+
+        return authResult.Match(
+            authResult =>
+            {
+                logger.LogInformation($"User registered: {request.Email}");
+                return Ok(mapper.Map<AuthenticationResponse>(authResult));
+            },
+            errors =>
+            {
+                logger.LogError($"Registration failed for user: {request.Email}, Errors: {string.Join(", ", errors.Select(e => e.Description))}");
+                return Problem(errors);
+            });
     }
-
-    var authResult = await mediatr.Send(mapper
-        .Map<RegisterCommand>((request, baseUrl, image)));
-
-    return authResult.Match(
-        authResult => Ok(mapper.Map<AuthenticationResponse>(authResult)),
-        errors => Problem(errors));
-}
 
     [HttpGet("confirm-email")]
     public async Task<IActionResult> ConfirmEmailAsync([FromQuery] ConfirmEmailRequest request)
     {
+        logger.LogInformation($"Attempting to confirm email for user: {request.UserId}");
+
         var confirmEmailResult = await mediatr.Send(mapper.Map<ConfirmEmailCommand>(request));
 
         return confirmEmailResult.Match(
-            confirmResult => Redirect("http://192.168.0.13:5173"),
-            errors => Problem(errors));
+            confirmResult =>
+            {
+                logger.LogInformation($"Email confirmed for user: {request.UserId}");
+                return Redirect("http://3.72.8.43:5173");
+            },
+            errors =>
+            {
+                logger.LogError($"Email confirmation failed for user: {request.UserId}, Errors: {string.Join(", ", errors.Select(e => e.Description))}");
+                return Problem(errors);
+            });
     }
 
     [HttpGet("resend-confirmation-email")]
     public async Task<IActionResult> ResendConfirmationEmailAsync([FromQuery] ResendConfirmEmailRequest request)
     {
+        logger.LogInformation($"Attempting to resend confirmation email for user: {request.Email}");
+
         var baseUrl = configuration.GetRequiredSection("HostSettings:ClientURL").Value;
         var resendConfirmationResult = await mediatr.Send(mapper
             .Map<ResendConfirmEmailCommand>((request, baseUrl)));
 
         return resendConfirmationResult.Match(
-            success => Ok("Confirmation email resent successfully"),
-            errors => Problem(errors));
+            success =>
+            {
+                logger.LogInformation($"Confirmation email resent for user: {request.Email}");
+                return Ok("Confirmation email resent successfully");
+            },
+            errors =>
+            {
+                logger.LogError($"Resending confirmation email failed for user: {request.Email}, Errors: {string.Join(", ", errors.Select(e => e.Description))}");
+                return Problem(errors);
+            });
     }
-
 
     [HttpPost("login")]
     public async Task<IActionResult> LoginAsync([FromBody] LoginRequest request)
     {
+        logger.LogInformation($"Attempting to authenticate user: {request.Email}");
+
         var query = mapper.Map<LoginQuery>(request);
         var authenticationResult = await mediatr.Send(query);
 
-        if (authenticationResult.IsError && authenticationResult
-                .FirstError == Errors.Authentication.InvalidCredentials)
+        if (authenticationResult.IsError && authenticationResult.FirstError == Errors.Authentication.InvalidCredentials)
         {
-            return Problem(statusCode: StatusCodes.Status401Unauthorized,
-                title: authenticationResult.FirstError.Description);
+            logger.LogWarning($"Authentication failed for user: {request.Email}, Reason: Invalid credentials");
+            return Problem(statusCode: StatusCodes.Status401Unauthorized, title: authenticationResult.FirstError.Description);
         }
 
         return authenticationResult.Match(
-            authenticationResult => Ok(mapper
-                .Map<AuthenticationResponse>(authenticationResult)),
-            errors => Problem(errors));
+            authenticationResult =>
+            {
+                logger.LogInformation($"User authenticated: {request.Email}");
+                return Ok(mapper.Map<AuthenticationResponse>(authenticationResult));
+            },
+            errors =>
+            {
+                logger.LogError($"Authentication failed for user: {request.Email}, Errors: {string.Join(", ", errors.Select(e => e.Description))}");
+                return Problem(errors);
+            });
     }
 
     [HttpGet("forgot-password")]
     public async Task<IActionResult> ForgotPasswordAsync([FromQuery] string email)
     {
+        logger.LogInformation($"Attempting to send forgot password email for user: {email}");
+
         var baseUrl = Request.Headers["Referer"].ToString();
 
         var query = new ForgotPasswordQuery(email, baseUrl);
@@ -108,14 +148,22 @@ public async Task<IActionResult> RegisterAsync([FromForm] RegisterRequest reques
         var forgotPasswordResult = await mediatr.Send(query);
 
         return forgotPasswordResult.Match(
-            forgotPasswordRes => Redirect("http://192.168.0.13:5173"),
-            errors => Problem(errors));
+            forgotPasswordRes =>
+            {
+                logger.LogInformation($"Forgot password email sent for user: {email}");
+                return Redirect("http://3.72.8.43:5173");
+            },
+            errors =>
+            {
+                logger.LogError($"Forgot password email failed for user: {email}, Errors: {string.Join(", ", errors.Select(e => e.Description))}");
+                return Problem(errors);
+            });
     }
 
     [HttpPost("reset-password")]
     public async Task<IActionResult> ResetPasswordAsync(ResetPasswordRequest request)
     {
-        Console.WriteLine($"Спроба скинути пароль для користувача з email: {request.Email}");
+        logger.LogInformation($"Attempting to reset password for user: {request.Email}");
 
         var baseUrl = configuration.GetRequiredSection("HostSettings:ClientURL").Value;
 
@@ -127,51 +175,66 @@ public async Task<IActionResult> RegisterAsync([FromForm] RegisterRequest reques
         return resetPasswordResult.Match(
             resetPasswordRes =>
             {
-                Console.WriteLine($"Пароль для користувача {request.Email} успішно скинуто.");
+                logger.LogInformation($"Password reset for user: {request.Email}");
                 return Ok(resetPasswordResult.Value);
             },
             errors =>
             {
-                Console.WriteLine($"Помилка при скиданні пароля для користувача {request.Email}.");
+                logger.LogError($"Password reset failed for user: {request.Email}, Errors: {string.Join(", ", errors.Select(e => e.Description))}");
                 return Problem(errors);
-            }
-        );
+            });
     }
-
 
     [HttpPost("change-email")]
     public async Task<IActionResult> ChangeEmailAsync([FromBody] ChangeEmailRequest request)
     {
+        logger.LogInformation($"Attempting to change email for user: {request.Email}");
+
         var baseUrl = configuration.GetRequiredSection("HostSettings:ClientURL").Value;
         var changeEmailCommand = mapper.Map<ChangeEmailCommand>(request);
         changeEmailCommand = changeEmailCommand with { BaseUrl = baseUrl };
         var changeEmailResult = await mediatr.Send(changeEmailCommand);
 
         return changeEmailResult.Match(
-            changeEmailRes => Ok(changeEmailResult.Value),
-            errors => Problem(errors));
+            changeEmailRes =>
+            {
+                logger.LogInformation($"Email changed for user: {request.Email}");
+                return Ok(changeEmailResult.Value);
+            },
+            errors =>
+            {
+                logger.LogError($"Email change failed for user: {request.Email}, Errors: {string.Join(", ", errors.Select(e => e.Description))}");
+                return Problem(errors);
+            });
     }
 
     [HttpPost("logout")]
     public async Task<IActionResult> LogoutAsync()
     {
+        logger.LogInformation("Attempting to log out user");
+
         var currentUserId = currentUserService.GetCurrentUserId();
         var logoutResult = await authenticationService.LogoutUserAsync(currentUserId);
 
         if (logoutResult.IsError)
         {
+            logger.LogError($"Logout failed for user: {currentUserId}, Errors: {string.Join(", ", logoutResult.Errors.Select(e => e.Description))}");
             return Problem(logoutResult.Errors);
         }
 
+        logger.LogInformation("User logged out successfully");
         return Ok("Logged out successfully");
     }
 
     [HttpGet("user-status/{userId}")]
     public async Task<IActionResult> GetUserStatusAsync(Guid userId)
     {
+        logger.LogInformation($"Attempting to get user status for user: {userId}");
+
         var user = await userManager.FindByIdAsync(userId.ToString());
         if (user == null)
         {
+            logger.LogWarning($"User not found: {userId}");
             return NotFound();
         }
 
@@ -181,13 +244,14 @@ public async Task<IActionResult> RegisterAsync([FromForm] RegisterRequest reques
             LastActive = user.LastActive
         };
 
+        logger.LogInformation($"User status retrieved for user: {userId}");
         return Ok(status);
     }
-
 
     [HttpGet("ping")]
     public IActionResult Ping()
     {
+        logger.LogInformation("Ping request received");
         return Ok(DateTime.Now);
     }
 }
